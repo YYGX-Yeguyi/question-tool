3f9f797fb013518740d7b40681fcb99f10388b94
YYGX-Yeguyi <22102188457@qq.com>
2026-04-20 09:13:21 +0800

    feat: 初始框架搭建

---
+++ question-tool-core/src/main/java/com/whiteabyss/questiontoolcore/QuestionToolCoreApplication.java
@@ -0,0 +1,13 @@
+package com.whiteabyss.questiontoolcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionToolCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestionToolCoreApplication.class, args);
+    }
+
+}
---
+++ question-tool-core/src/test/java/com/whiteabyss/questiontoolcore/QuestionToolCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whiteabyss.questiontoolcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionToolCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
